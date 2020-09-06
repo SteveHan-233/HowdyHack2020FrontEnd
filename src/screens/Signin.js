7a97163d243393b70f05262a83df4d830a1f61b4
Steve Han <stevehan2001@gmail.com>
2020-09-06 06:38:24 -0500

    added sign in

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,13 +1,79 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, TextInput } from 'react-native-gesture-handler';
 
 export default ({ setAuthenticated }) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={require('../../assets/tamu.png')} style={styles.logo} />
+      <Text style={styles.title}>
+        Please sign in with your TAMU email address to use the app
+      </Text>
+      <View style={styles.input}>
+        <TextInput
+          placeholder="email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.text_input}
+        />
+      </View>
+      <View style={styles.input}>
+        <TextInput
+          placeholder="password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.text_input}
+          secureTextEntry
+        />
+      </View>
       <TouchableOpacity onPress={() => setAuthenticated(true)}>
-        <Text>Sign in</Text>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#500000',
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 40,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+  },
+  title: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '100%',
+    padding: 10,
+    marginBottom: 20,
+  },
+  text_input: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#A32323',
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: 'white',
+  },
+});
